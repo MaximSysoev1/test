e3ca30add656e2ab9e15a981f550c3aac1e52fe7
Maxim Sysoev <time_amf@mail.ru>
2019-02-15 14:22:16 +0300

    taks

--- taskmvc/src/main/java/servlet/AddCat.java
+++ taskmvc/src/main/java/servlet/AddCat.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 public class AddCat extends HttpServlet {
     private final ValidateService logic = ValidateService.getInstance();
 
